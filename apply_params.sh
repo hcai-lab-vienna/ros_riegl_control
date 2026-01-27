46ebe8f0427c99bcf0aa8ba8bf2faef35232ce13
3ernhard <bernhard-hoerl@gmx.at>
2026-01-27 13:42:22 +0100

    try fix compile

---
+++ apply_params.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ $# -eq 0 ]; then
+	echo "please provided param file as argument"
+fi
+SOURCE_FILE="$1"
+TARGET_PATH="install/riegl_vz/share/riegl_vz/config/"
+TARGET_FILE="install/riegl_vz/share/riegl_vz/config/params.yaml"
+if [ -f "$SOURCE_FILE" ]; then
+	mkdir -p "$TARGET_PATH"
+	cp "$SOURCE_FILE" "$TARGET_FILE"
+else
+	echo "$SOURCE_FILE does not exist."
+fi
