ed453e2f5f4f785cdd1ffc127479ce0799fc8193
3ernhard <bernhard-hoerl@gmx.at>
2026-02-05 15:18:19 +0100

    fix fixes to fix unfixed things

--- fix_riegl_typo.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cp fix/project.py install/riegl_vz/lib/python3.*/site-packages/riegl_vz/project.py
---
+++ fixes/fix_riegl_typo.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cp ./project.py ../install/riegl_vz/lib/python3.*/site-packages/riegl_vz/project.py
