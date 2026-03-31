b5b8ba228afbb8f7494251670eab1207090d3379
3ernhard <bernhard-hoerl@gmx.at>
2026-03-31 14:37:30 +0200

    added imu

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ rm -rf build install log
 git submodule update --init --recursive
 
 ./fixes/fix_riegl_typo.sh
-colcon build --packages-ignore hcai_nav_bringup basic_pure_pursuit_controller
+colcon build --packages-ignore hcai_nav_bringup basic_pure_pursuit_controller ros_imu
 
-colcon build --symlink-install --packages-select hcai_nav_bringup basic_pure_pursuit_controller
+colcon build --symlink-install --packages-select hcai_nav_bringup basic_pure_pursuit_controller ros_imu
