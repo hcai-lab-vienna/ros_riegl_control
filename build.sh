c8834be009005ab0a0a298f0fc5335ba2049aa9d
3ernhard <bernhard-hoerl@gmx.at>
2026-03-31 10:23:56 +0200

    build.sh

--- build.sh
+++ build.sh
@@ -4,11 +4,7 @@ rm -rf build install log
 
 git submodule update --init --recursive
 
-colcon build --packages-select ugv_sdk
-colcon build --packages-select bunker_ros2
-
-colcon build --packages-select ros-riegl-vz
+colcon build --packages-ignore hcai_nav_bringup basic_pure_pursuit_controller
 ./fixes/fix_riegl_typo.sh
 
-colcon build --packages-select hcai_nav_bringup --symlink-install
-colcon build --packages-select basic_pure_pursuit_controller --symlink-install
+colcon build --symlink-install --packages-select hcai_nav_bringup basic_pure_pursuit_controller
