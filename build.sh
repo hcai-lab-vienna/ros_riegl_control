ecacc3e018574ee7dc31d631eb3853d49c4b659f
3ernhard <bernhard-hoerl@gmx.at>
2026-05-28 14:34:17 +0200

    removed automatic fixes

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ rm -rf build install log
 
 git submodule update --init --recursive
 
-./fixes/fix_riegl_typo.sh
 colcon build --packages-ignore hcai_nav_bringup basic_pure_pursuit_controller ros_imu
 
 colcon build --symlink-install --packages-select hcai_nav_bringup basic_pure_pursuit_controller ros_imu
